9a20e8c3c46c6063afaac5047951b4eeac7ad0bc
ValkyrieUK <seanisgone@Sean-Holdens-MacBook-Air.local>
2013-10-09 08:57:51 +0100

    app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nba::Application.config.secret_key_base = '72f858245991235ab6555b1f531569afa01285ad251b0f12ae278ad9adf4aa818d817797acd7d549500099a0603898fb936e83017f668945f8588f199a4bd0f6'
