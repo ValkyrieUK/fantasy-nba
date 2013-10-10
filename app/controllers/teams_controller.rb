db0b4ab5b3b13a2448ba7abecd5bed5f67d3fc34
ValkyrieUK <seanisgone@Sean-Holdens-MacBook-Air.local>
2013-10-10 14:44:19 +0100

    Show method

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,24 +1,27 @@
 class TeamsController < ApplicationController
 
   def index
+    @teams = Team.all
   end
 
   def new
   end
 
   def create
-  @team = Team.new team_params
- 
-  @team.save
-  redirect_to @team
-end
- 
-private
+    @team = Team.new team_params
+   
+    @team.save
+    redirect_to @team
+  end
+
+  def show
+   @team = Team.find(params[:id])
+  end 
+
+  private
+  
   def team_params
     params.require(:team).permit(:name, :hometown)
   end
 
-  def show
-  @team = Team.find(params[:id])
-end 
 end
