569b334aa49d4ecedbf17cd4d15aa854176ad02f
ValkyrieUK <seanisgone@Bradley-Allens-MacBook-Air.local>
2013-10-14 18:46:26 +0100

    Added a user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Nba::Application.routes.draw do
   devise_for :users
+  resources :users, :only => [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+
   root :to => 'home#index'
+  get 'users/:id' => 'users#show'
   resources :teams do
     collection do
       get 'remove_all'
