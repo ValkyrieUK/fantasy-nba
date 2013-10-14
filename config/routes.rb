9302eb6371fad3cdb87500b176cb6abed74f40ed
ValkyrieUK <seanisgone@Bradley-Allens-MacBook-Air.local>
2013-10-14 21:00:51 +0100

    User Profiles work, index needs fixing

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Nba::Application.routes.draw do
   devise_for :users
-  resources :users, :only => [:show]
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
 
   root :to => 'home#index'
-  get 'users/:id' => 'users#show'
+  # get 'users/:id' => 'users#show'
   resources :teams do
     collection do
       get 'remove_all'
