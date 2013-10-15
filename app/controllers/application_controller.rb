54c08dd2f5be5971bb8bc840a8e4939103a2ea01
ValkyrieUK <seanisgone@Bradley-Allens-MacBook-Air.local>
2013-10-15 13:41:18 +0100

    Fixed updating name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,23 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(devise_required) }
+    devise_parameter_sanitizer.for(:sign_up){ |u| u.permit(devise_required) }
+    devise_parameter_sanitizer.for(:account_update){ |u| u.permit(devise_update_required) }
   end
 
   def devise_required
     [
       :email,
       :password,
+      :first_name,
+      :last_name
+    ]
+  end
+
+  def devise_update_required
+    [
+      :email,
+      :current_password,
       :first_name,
       :last_name
     ]
